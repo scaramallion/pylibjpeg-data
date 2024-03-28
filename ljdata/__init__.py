8253566715800a7fc3d4d949abab102c8172bca0
scaramallion <scaramallion@users.noreply.github.com>
2024-03-29 09:57:58 +1100

    Blacken and add JPEG with Adobe v101 APP marker (#17)

--- ljdata/__init__.py
+++ ljdata/__init__.py
@@ -7,5 +7,5 @@ from .manager import get_indexed_datasets
 
 
 DATA_DIRECTORY = Path(__file__).resolve().parent
-DICOM_DIRECTORY = DATA_DIRECTORY / 'ds'
-JPEG_DIRECTORY = DATA_DIRECTORY / 'jpg'
+DICOM_DIRECTORY = DATA_DIRECTORY / "ds"
+JPEG_DIRECTORY = DATA_DIRECTORY / "jpg"
--- ljdata/manager.py
+++ ljdata/manager.py
@@ -1,8 +1,8 @@
-
 from pathlib import Path
 
 try:
     from pydicom import dcmread
+
     HAS_PYDICOM = True
 except ImportError:
     HAS_PYDICOM = False
@@ -22,24 +22,24 @@ from ljdata.ds import (
 )
 
 DATA_DIRECTORY = Path(__file__).resolve().parent
-DS_DIR = DATA_DIRECTORY / 'ds'
-JPG_DIR = DATA_DIRECTORY / 'jpg'
+DS_DIR = DATA_DIRECTORY / "ds"
+JPG_DIR = DATA_DIRECTORY / "jpg"
 
 
 UIDS = {
-    '1.2.840.10008.1.2':       'LittleEndianImplicit',
-    '1.2.840.10008.1.2.1':     'LittleEndianExplicit',
-    '1.2.840.10008.1.2.4.50':  'JPEGBaseline',
-    '1.2.840.10008.1.2.4.51':  'JPEGExtended',
-    '1.2.840.10008.1.2.4.57':  'JPEGLossless',
-    '1.2.840.10008.1.2.4.70':  'JPEGLosslessSV1',
-    '1.2.840.10008.1.2.4.80':  'JPEGLSLossless',
-    '1.2.840.10008.1.2.4.81':  'JPEGLS',
-    '1.2.840.10008.1.2.4.90':  'JPEG2000Lossless',
-    '1.2.840.10008.1.2.4.91':  'JPEG2000',
-    '1.2.840.10008.1.2.4.201': 'HTJ2KLossless',
-    '1.2.840.10008.1.2.4.203': 'HTJ2K',
-    '1.2.840.10008.1.2.5':     'RLELossless',
+    "1.2.840.10008.1.2": "LittleEndianImplicit",
+    "1.2.840.10008.1.2.1": "LittleEndianExplicit",
+    "1.2.840.10008.1.2.4.50": "JPEGBaseline",
+    "1.2.840.10008.1.2.4.51": "JPEGExtended",
+    "1.2.840.10008.1.2.4.57": "JPEGLossless",
+    "1.2.840.10008.1.2.4.70": "JPEGLosslessSV1",
+    "1.2.840.10008.1.2.4.80": "JPEGLSLossless",
+    "1.2.840.10008.1.2.4.81": "JPEGLS",
+    "1.2.840.10008.1.2.4.90": "JPEG2000Lossless",
+    "1.2.840.10008.1.2.4.91": "JPEG2000",
+    "1.2.840.10008.1.2.4.201": "HTJ2KLossless",
+    "1.2.840.10008.1.2.4.203": "HTJ2K",
+    "1.2.840.10008.1.2.5": "RLELossless",
 }
 
 
@@ -59,7 +59,7 @@ def get_datasets(uid, as_dataset=False):
     Union[List[Path], List[Dataset]]
     """
     subdir = UIDS[uid]
-    fnames = get_indices('ds')[subdir].keys()
+    fnames = get_indices("ds")[subdir].keys()
     fpaths = [DS_DIR / subdir / fname for fname in fnames]
     if as_dataset:
         if not HAS_PYDICOM:
@@ -72,11 +72,11 @@ def get_datasets(uid, as_dataset=False):
 
 def get_indexed_datasets(uid):
     subdir = UIDS[uid]
-    index = get_indices('ds')[subdir]
+    index = get_indices("ds")[subdir]
     fnames = index.keys()
     for fname in fnames:
         fpath = DS_DIR / subdir / fname
-        index[fname]['ds'] = dcmread(fpath)
+        index[fname]["ds"] = dcmread(fpath)
 
     return index
 
@@ -85,12 +85,12 @@ def get_from_parameter(index, keyword, value):
     matches = []
     for kk, vv in index.items():
         if keyword in vv and vv[keyword] == value:
-            matches.append(index[kk]['ds'])
+            matches.append(index[kk]["ds"])
 
     return matches
 
 
-def get_indices(index_type='ds'):
+def get_indices(index_type="ds"):
     """Return a :class:`dict` containing all the indices for `index_type`.
 
     Parameters
@@ -98,17 +98,17 @@ def get_indices(index_type='ds'):
     index_type : str
         The index type to get, one of ``'ds'``.
     """
-    if index_type == 'ds':
+    if index_type == "ds":
         return {
-            'LittleEndianImplicit' : LittleEndianImplicit_IDX,
-            'LittleEndianExplicit' : LittleEndianExplicit_IDX,
-            'JPEGBaseline' : JPEGBaseline_IDX,
-            'JPEGExtended' : JPEGExtended_IDX,
-            'JPEGLossless' : JPEGLossless_IDX,
-            'JPEGLosslessSV1' : JPEGLosslessSV1_IDX,
-            'JPEGLSLossless' : JPEGLSLossless_IDX,
-            'JPEGLS' : JPEGLS_IDX,
-            'JPEG2000Lossless' : JPEG2000Lossless_IDX,
-            'JPEG2000' : JPEG2000_IDX,
-            'RLELossless' : RLELossless_IDX,
+            "LittleEndianImplicit": LittleEndianImplicit_IDX,
+            "LittleEndianExplicit": LittleEndianExplicit_IDX,
+            "JPEGBaseline": JPEGBaseline_IDX,
+            "JPEGExtended": JPEGExtended_IDX,
+            "JPEGLossless": JPEGLossless_IDX,
+            "JPEGLosslessSV1": JPEGLosslessSV1_IDX,
+            "JPEGLSLossless": JPEGLSLossless_IDX,
+            "JPEGLS": JPEGLS_IDX,
+            "JPEG2000Lossless": JPEG2000Lossless_IDX,
+            "JPEG2000": JPEG2000_IDX,
+            "RLELossless": RLELossless_IDX,
         }
